08d279e81a659ee2df4f4af50f0abf4f33fdf4e4
Ivan I. Zemchikov <zemchikov0@gmail.com>
2022-05-26 09:23:19 +0400

    some pending changes

--- app/models/weather_samara.rb
+++ app/models/weather_samara.rb
@@ -5,34 +5,46 @@ class WeatherSamara < ApplicationRecord
   base_uri "http://dataservice.accuweather.com/currentconditions/v1/290396/historical/24?apikey=JE3RW8gV34wyeX7VWT9DZlv9YerZdtql&language=ru"
   format :json
 
-  #call the api with HTTParty and parse the JSON response
-  def self.call
+  attr_accessor :body
+
+  def new
     response = HTTParty.get(base_uri)
-    body = JSON.parse(response.body)
+    @body = JSON.parse(response.body)
     @historical =
-      body.map do |element|
+      @body.map do |element|
         Hash[time: element["LocalObservationDateTime"],
              temp: element["Temperature"]]
       end
   end
 
-  def self.historical
+  #call the api with HTTParty and parse the JSON response
+  # def self.call
+  #   response = HTTParty.get(base_uri)
+  #   body = JSON.parse(response.body)
+  #   @historical =
+  #     body.map do |element|
+  #       Hash[time: element["LocalObservationDateTime"],
+  #            temp: element["Temperature"]]
+  #     end
+  # end
+
+  def historical
     @historical
   end
 
-  def self.historical_max
+  def historical_max
     @historical.map { |el| el[:temp]["Metric"]["Value"] }.max
   end
 
-  def self.historical_min
+  def historical_min
     @historical.map { |el| el[:temp]["Metric"]["Value"] }.min
   end
 
-  def self.current
+  def current
     @historical[0]
   end
 
-  def self.historical_avg
+  def historical_avg
     (@historical.map { |el| el[:temp]["Metric"]["Value"] }.reduce(:+).to_f / @historical.size).round(1)
   end
 
